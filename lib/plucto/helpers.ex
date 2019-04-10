77b4bc30a360e5339da465ac9211ab80650af814
Mike McCall <mike.mccall@nothingspecificinc.com>
2019-04-10 09:10:50 -0700

    adding page, next, prev, first, last helper

--- lib/plucto/helpers.ex
+++ lib/plucto/helpers.ex
@@ -1,6 +1,59 @@
 defmodule Plucto.Helpers do
   alias Plucto.Page
 
+  def page_url(
+        page_num,
+        %Page{params: params},
+        %Plug.Conn{host: host, scheme: scheme, request_path: req_path},
+        opts \\ []
+      ) do
+    query = %{params | "page" => page_num} |> URI.encode_query()
+    path = "#{req_path}?#{query}"
+
+    case Keyword.get(opts, :host, false) do
+      false -> path
+      _ -> "#{scheme}://#{host}#{path}"
+    end
+  end
+
+  def next(
+        %Page{params: params} = page,
+        %Plug.Conn{} = conn,
+        opts \\ []
+      ) do
+    {page_num, _} = Integer.parse(params["page"])
+
+    (page_num + 1)
+    |> page_url(page, conn, opts)
+  end
+
+  def previous(
+        %Page{params: params} = page,
+        %Plug.Conn{} = conn,
+        opts \\ []
+      ) do
+    {page_num, _} = Integer.parse(params["page"])
+
+    (page_num - 1)
+    |> page_url(page, conn, opts)
+  end
+
+  def last(
+        %Page{last_page: last_page} = page,
+        %Plug.Conn{} = conn,
+        opts \\ []
+      ) do
+    page_url(last_page, page, conn, opts)
+  end
+
+  def first(
+        %Page{} = page,
+        %Plug.Conn{} = conn,
+        opts \\ []
+      ) do
+    page_url(1, page, conn, opts)
+  end
+
   def range(%Page{} = page, padding \\ 3) do
     first.._ = left_range(page, padding)
     _..last = right_range(page, padding)
